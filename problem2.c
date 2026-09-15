e6117be058451d786857ceaf5b2337e5706cd333
AnshulAmanlove <anshulamanpvt@gmail.com>
2026-09-16 00:05:15 +0530

    Add input and output for integer, float, and double

--- problem2.c
+++ problem2.c
@@ -1,6 +1,16 @@
 # include <stdio.h>
 int main()
 {
+  //to print the values from the variable
+  int a;
+  float f;
+  double d;
+  printf("enter the integer , float and a double value simultaneously");
+  scanf("%d %f %lf", &a ,&f , &d);
+    
+  printf("the integer value is %d\n",a);
+  printf("the float value is %f\n",f);
+  printf("the double value is %lf",d);
   
   return 0;
 }
