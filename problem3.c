b4d0800854f869bde598a93c39970f8f6b77dcca
AnshulAmanlove <anshulamanpvt@gmail.com>
2026-09-16 01:48:23 +0530

    Add comments for user input steps in problem3.c
    Added comments detailing the steps for user input and output.

--- problem3.c
+++ problem3.c
@@ -24,3 +24,16 @@ int main()
   
   return 0;
 }
+
+/*Step 1: Start.
+Step 2: Declare character variables fn and sn, and integer variable yob.
+Step 3: Print "enter your forename:".
+Step 4: Read input into variable fn.
+Step 5: Print "enter your surname".
+Step 6: Read input into variable sn.
+Step 7: Print "enter your year of birth".
+Step 8: Read input into variable yob.
+Step 9: Print "forename : %c\n" displaying the value of fn.
+Step 10: Print "surname: %c\n" displaying the value of sn.
+Step 11: Print "year of birth: %d\n" displaying the value of yob.
+Step 12: Stop.*/
