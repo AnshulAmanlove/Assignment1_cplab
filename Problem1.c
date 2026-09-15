5267747b1d031edf4bcbfe06e3d8cc6edf426a58
AnshulAmanlove <anshulamanpvt@gmail.com>
2026-09-15 23:47:34 +0530

    Correct main function syntax and comment style
    Fixed main function declaration and updated comments.

--- Problem1.c
+++ Problem1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main
+int main()
 {
-  \\enter the credentials of a student
+  /*enter the credentials of a student*/
   printf("the name of the student is anshul aman \n");
   printf("the roll number of the student is 26EE1008 \n");
   printf("the subject of the student is electrical engineering");
