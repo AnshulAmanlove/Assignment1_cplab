724029b2359ea0af44d31e8e110fc7d8c6a3669e
AnshulAmanlove <anshulamanpvt@gmail.com>
2026-09-16 01:38:59 +0530

    Document flowchart for student information printing
    Added flowchart comments for student information display.

--- Problem1.c
+++ Problem1.c
@@ -17,3 +17,24 @@ Step 2: Print the student's name ("the name of the student is anshul aman")
 Step 3: Print the student's roll number ("the roll number of the student is 26EE1008")
 Step 4: Print the student's subject ("the subject of the student is electrical engineering")
 Step 5: Stop */
+
+       ( Start )  <-- Oval shape
+           |
+           v
+  /-------------------------------------------------------\
+ / Print "the name of the student is anshul aman \n"       \  <-- Parallelogram
+/---------------------------------------------------------/
+           |
+           v
+  /-------------------------------------------------------\
+ / Print "the roll number of the student is 26EE1008 \n"   \  <-- Parallelogram
+/---------------------------------------------------------/
+           |
+           v
+  /--------------------------------------------------------------\
+ / Print "the subject of the student is electrical engineering"   \ <-- Parallelogram
+/----------------------------------------------------------------/
+           |
+           v
+       ( Stop )   <-- Oval shape
+
