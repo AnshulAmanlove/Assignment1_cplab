ce0d635cfa23b5f046de8edb79e727310276a8c3
AnshulAmanlove <anshulamanpvt@gmail.com>
2026-09-16 01:42:06 +0530

    Add comments for program steps in problem2.c
    Added comments to explain the steps of the program.

--- problem2.c
+++ problem2.c
@@ -14,3 +14,12 @@ int main()
   
   return 0;
 }
+
+/*Step 1: Start.
+Step 2: Declare an integer variable a, a float variable f, and a double variable d.
+Step 3: Print a message to the user: "enter the integer, float and a double value simultaneously".
+Step 4: Read three values entered by the user and store them in the variables a, f, and d.
+Step 5: Print a message displaying the integer value: "the integer value is %d\n" where %d is the value of a.
+Step 6: Print a message displaying the float value: "the float value is %f\n" where %f is the value of f.
+Step 7: Print a message displaying the double value: "the double value is %lf" where %lf is the value of d.
+Step 8: Stop.*/
